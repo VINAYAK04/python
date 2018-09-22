7f3697ec3438a9169a4fe8f572b576256770b253
VINAYAK04 <43026214+VINAYAK04@users.noreply.github.com>
2018-09-22 12:52:31 +0530

    Add files via upload

---
+++ if.py
@@ -0,0 +1,8 @@
+#FIND GREATEST OF TWO NUMBERS
+a=int(input("enter value of a: "))
+b=int(input("enter value of b: "))
+#checking the condition
+if(a>b):
+	 print("A is greater than B")
+else:
+	print("B is greater than A")
---
+++ marks.py
@@ -0,0 +1,14 @@
+#finding grades 
+a=int(input("value of a: "))
+#checking condition
+if(a>=80):
+ 	 print("A")
+elif(a>=70):
+ 	print("B")
+elif(a>=60):
+ 	print("C")
+elif(a>=50):
+ 	print("D")
+else:
+ 	print("fail")
+
